e08a4dfd9d37aa4035d07a3834969f4d17c083f4
Brian Leathem <bleathem@gmail.com>
2015-09-09 09:36:44 -0700

    Removed the initial PoC WFM modules: person, address, google-maps

--- portal/app/main.js
+++ portal/app/main.js
@@ -5,14 +5,11 @@ var angular = require('angular');
 angular.module('app', [
   require('angular-ui-router')
 , require('wfm-mediator')
-, require('wfm-person')
 , require('wfm-workorder')
-, require('wfm-address')
-, require('wfm-google-maps')
+, require('wfm-workflow')
 , require('./home/home')
 , require('./workorder/workorder')
 , require('./workorder/workorder-list')
-, require('./workflow/workflow')
 ])
 
 .config(function($stateProvider, $urlRouterProvider) {
