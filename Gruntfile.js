64c6102feccc73742d3b0fc92122072ec646e90a
Brian Leathem <bleathem@gmail.com>
2016-02-03 08:11:45 -0800

    Further fixes to the js watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -77,7 +77,6 @@ module.exports = function (grunt) {
     // Watches files for changes and runs tasks based on the changed files
     watch: {
       js: {
-<<<<<<< HEAD
         files: ['<%= app.dist %>/app/bundle.js'],
         options: {
           livereload: '<%= connect.options.livereload %>'
