158687614b8978089ce2d74e392d8afcdfe9fced
Brian Leathem <bleathem@gmail.com>
2016-02-10 15:54:30 -0800

    Made the map dynamic

--- www/app/map/map.js
+++ www/app/map/map.js
@@ -14,6 +14,11 @@ angular.module('app.map', [
       data: {
         columns: 2
       },
+      resolve: {
+        workorders: function(workorderManager) {
+          return workorderManager.list();
+        }
+      },
       views: {
         content: {
           templateUrl: 'app/map/map.tpl.html',
@@ -24,22 +29,45 @@ angular.module('app.map', [
     })
 })
 
-.controller('mapController', function (mediator) {
-  function init_map() {
+.controller('mapController', function ($window, $document, $timeout, workorders) {
+  function initMap() {
     var myOptions = {
       zoom:11,
       center:new google.maps.LatLng(49.2075439,-123.1108627),
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
-    var marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(49.2075439,-123.1108627)});
-    var infowindow = new google.maps.InfoWindow({content:'<strong>Workorder #1234567</strong><br>Vancouver, BC<br>'});
-    google.maps.event.addListener(marker, 'click', function(){
-      infowindow.open(map,marker);
+    workorders.forEach(function(workorder) {
+      var x = 49.2075439 + (Math.random() - 0.5) * 0.1;
+      var y =-123.1108627 + (Math.random() - 0.5) * 0.1;
+      var marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(x, y)});
+      var infowindow = new google.maps.InfoWindow({content:'<strong>Workorder #'+workorder.id+'</strong><br>Vancouver, BC<br>'});
+      google.maps.event.addListener(marker, 'click', function(){
+        infowindow.open(map,marker);
+      });
     });
-    infowindow.open(map,marker);
+  };
+
+  function resizeMap() {
+    var map = $document[0].getElementById('gmap_canvas');
+    var parent = map.parentElement.parentElement;
+    var height = parent.clientHeight;
+    var width = parent.clientWidth;
+    map.style.height = height + 'px';
+    map.style.width = width + 'px';
+
+    console.log('Map dimensions:', width, height);
+    google.maps.event.trigger(map, 'resize');
+  };
+
+  $window.onresize = function() { // TODO: throttle this
+    resizeMap();
   }
-  init_map();
+
+  initMap();
+  $timeout(function() {
+    resizeMap()
+  });
 })
 
 ;
