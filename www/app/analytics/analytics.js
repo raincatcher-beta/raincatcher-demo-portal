a36c99291b874ddbc10b540dd0415acdf2b7daab
Brian Leathem <bleathem@gmail.com>
2016-02-12 13:12:24 -0800

    Added some chart placeholders

--- www/app/analytics/analytics.js
+++ www/app/analytics/analytics.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var d3 = require('d3')
+var c3 = require('c3')
+
 module.exports = 'app.analytics';
 
 angular.module('app.analytics', [
@@ -24,6 +27,62 @@ angular.module('app.analytics', [
 })
 
 .controller('analyticsController', function () {
+  var barChart = c3.generate({
+    bindto: '#bar-chart',
+    size: {
+      width: 450
+    },
+    data: {
+      columns: [
+        ['data1', 30, 200, 100],
+        ['data2', 130, 100, 140],
+        ['data3', 130, 150, 200]
+      ],
+      type: 'bar'
+    },
+    bar: {
+      width: {
+        ratio: .8
+      }
+    }
+  });
+
+  var pieChart = c3.generate({
+    bindto: '#pie-chart',
+    size: {
+      width: 450
+    },
+    data: {
+        // iris data from R
+        columns: [
+            ["setosa", 0.2, 0.2, 0.2, 0.2, 0.2, 0.4, 0.3, 0.2, 0.2, 0.1, 0.2, 0.2, 0.1, 0.1, 0.2, 0.4, 0.4, 0.3, 0.3, 0.3, 0.2, 0.4, 0.2, 0.5, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.2, 0.4, 0.1, 0.2, 0.2, 0.2, 0.2, 0.1, 0.2, 0.2, 0.3, 0.3, 0.2, 0.6, 0.4, 0.3, 0.2, 0.2, 0.2, 0.2],
+            ["versicolor", 1.4, 1.5, 1.5, 1.3, 1.5, 1.3, 1.6, 1.0, 1.3, 1.4, 1.0, 1.5, 1.0, 1.4, 1.3, 1.4, 1.5, 1.0, 1.5, 1.1, 1.8, 1.3, 1.5, 1.2, 1.3, 1.4, 1.4, 1.7, 1.5, 1.0, 1.1, 1.0, 1.2, 1.6, 1.5, 1.6, 1.5, 1.3, 1.3, 1.3, 1.2, 1.4, 1.2, 1.0, 1.3, 1.2, 1.3, 1.3, 1.1, 1.3],
+            ["virginica", 2.5, 1.9, 2.1, 1.8, 2.2, 2.1, 1.7, 1.8, 1.8, 2.5, 2.0, 1.9, 2.1, 2.0, 2.4, 2.3, 1.8, 2.2, 2.3, 1.5, 2.3, 2.0, 2.0, 1.8, 2.1, 1.8, 1.8, 1.8, 2.1, 1.6, 1.9, 2.0, 2.2, 1.5, 1.4, 2.3, 2.4, 1.8, 1.8, 2.1, 2.4, 2.3, 1.9, 2.3, 2.5, 2.3, 1.9, 2.0, 2.3, 1.8],
+        ],
+        type : 'pie',
+        onclick: function (d, i) { console.log("onclick", d, i); },
+        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+    }
+  });
+
+  var areaChart = c3.generate({
+    bindto: '#area-chart',
+    size: {
+      width: 450
+    },
+    data: {
+      columns: [
+        ['data1', 300, 350, 300, 0, 0, 0],
+        ['data2', 130, 100, 140, 200, 150, 50]
+      ],
+    types: {
+      data1: 'area',
+      data2: 'area-spline'
+    }
+  }
+});
+
 })
 
 ;
