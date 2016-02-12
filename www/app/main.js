8d810afb8627789dcddaf0f6cf765b93b26ec189
Brian Leathem <bleathem@gmail.com>
2016-02-12 13:16:38 -0800

    Removed the settings menu

--- www/app/main.js
+++ www/app/main.js
@@ -26,7 +26,6 @@ angular.module('app', [
 , require('./schedule/schedule')
 , require('./map/map')
 , require('./analytics/analytics')
-, require('./settings/settings')
 ])
 
 .config(function($stateProvider, $urlRouterProvider) {
