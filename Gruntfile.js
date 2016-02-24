979794a797608ef38f331be9f999d12c32d31d8f
Brian Leathem <bleathem@gmail.com>
2016-02-24 13:46:54 -0800

    Fixed the html watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -83,7 +83,7 @@ module.exports = function (grunt) {
         }
       },
       html: {
-        files: ['<%= app.src %>/{,*/}*.html'],
+        files: ['<%= app.src %>/**/*.html'],
         tasks: ['copy:static']
       },
       sass: {
