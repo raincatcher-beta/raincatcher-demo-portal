493db598d40c670a63ac0ce4ef621ab67b604f71
Brian Leathem <bleathem@gmail.com>
2016-03-30 16:36:32 -0700

    List and detail pages for files

---
+++ src/app/file/file.js
@@ -0,0 +1,84 @@
+/**
+* CONFIDENTIAL
+* Copyright 2016 Red Hat, Inc. and/or its affiliates.
+* This is unpublished proprietary source code of Red Hat.
+**/
+'use strict';
+
+var _ = require('lodash');
+
+module.exports = 'app.file';
+
+angular.module('app.file', [
+  'ui.router'
+, 'wfm.core.mediator'
+])
+
+.config(function($stateProvider) {
+  $stateProvider
+    .state('app.file', {
+      url: '/files',
+      resolve: {
+        files: function(fileClient) {
+          return fileClient.list();
+        },
+        workerMap: function(userClient) {
+          return userClient.list().then(function(files) {
+            return files.reduce(function(map, worker) {
+              map[worker.id] = worker;
+              return map;
+            }, {});
+          });
+        }
+      },
+      views: {
+        column2: {
+          templateUrl: 'app/file/file-list.tpl.html',
+          controller: 'FileListController as ctrl',
+        },
+        'content': {
+          templateUrl: 'app/file/empty.tpl.html'
+        }
+      }
+    })
+    .state('app.file.detail', {
+      url: '/detail/:fileUid',
+      resolve: {
+        file: function($stateParams, files) {
+          return files.filter(function(file) {
+            return file.uid = $stateParams.fileUid;
+          })[0];
+        }
+      },
+      views: {
+        'content@app': {
+          templateUrl: 'app/file/file-detail.tpl.html',
+          controller: 'FileController as ctrl'
+        }
+      }
+    })
+})
+
+.run(function($state, mediator) {
+  mediator.subscribe('file:selected', function(file) {
+    $state.go('app.file.detail', {
+      fileUid: file.uid},
+      { reload: true }
+    );
+  });
+})
+
+.controller('FileListController', function ($scope, files, workerMap) {
+  var self = this;
+  $scope.$parent.selected = {id: null};
+  self.files = files;
+  self.workerMap = workerMap;
+})
+
+.controller('FileController', function ($scope, file, workerMap) {
+  var self = this;
+  $scope.$parent.selected = {id: file.id};
+  self.file = file;
+  self.workerMap = workerMap;
+})
+;
